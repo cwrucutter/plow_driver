6f83f44505ac5c860f936e8f399169c26e613967
Matthew Klein <ggallin08@gmail.com>
2015-01-10 20:14:05 -0500

    Wrote driver for plow. Limited in that it only takes integer number from 0-45.

---
+++ src/plow_driver.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2014, Matthew Klein
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following
+# disclaimer in the documentation and/or other materials provided
+# with the distribution.
+# * Neither the names of the authors nor the names of their
+# affiliated organizations may be used to endorse or promote products derived
+# from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rospy
+from std_msgs.msg import UInt8
+
+import serial, struct
+
+class plow_driver(object):
+  def __init__(self):
+    #ROS init
+    rospy.init_node('plow_driver')
+    rospy.Subscriber('/plow/angle', UInt8, self.plowAngleSubCB)
+
+    plowPort = rospy.get_param('~port','/dev/ttyACM0')
+    plowBaudrate = rospy.get_param('~baud',9600)
+
+    self.plowSerial = serial.Serial(port=plowPort,baudrate=plowBaudrate, timeout = 0.01)
+
+  def plowAngleSubCB(self, msg):
+    self.plowSerial.write(struct.pack("B",min(45,max(0,msg.data))))
+
+  def run(self):
+    rospy.spin()
+
+if __name__ == "__main__":
+  pd = plow_driver()
+  pd.run()
